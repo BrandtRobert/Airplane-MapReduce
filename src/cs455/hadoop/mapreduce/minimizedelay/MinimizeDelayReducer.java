94a47bcb962d1832fde27c20310725f78ffd7fbe
Brandt <breutiman@gmail.com>
2018-03-29 16:27:43 -0600

    Used percentage instead of total delays

--- src/cs455/hadoop/mapreduce/minimizedelay/MinimizeDelayReducer.java
+++ src/cs455/hadoop/mapreduce/minimizedelay/MinimizeDelayReducer.java
@@ -8,13 +8,17 @@ import org.apache.hadoop.mapreduce.Reducer;
 public class MinimizeDelayReducer extends Reducer <Text, IntWritable, Text, IntWritable> {
 	public void reduce (Text key, Iterable<IntWritable> delays, Context context) {
 		try {
+			int numDelays = 0;
 			int count = 0;
 			for (IntWritable val : delays) {
-				count += val.get();
+				numDelays += val.get();
+				count++;
 			}
+			double percentage = ((double) numDelays / (double) count) * 100.0;
+			int output = (int) Math.round(percentage);
 			// Trim off the M-, D-, or T- prefix
 			Text trimKey = new Text(key.toString().substring(2));
-			context.write(trimKey, new IntWritable(count));
+			context.write(trimKey, new IntWritable(output));
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {

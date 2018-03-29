5c1717aa87df58b665183f6832ea50f0695f7dd8
Brandt <breutiman@gmail.com>
2018-03-29 16:21:46 -0600

    Added map method for airports

--- src/cs455/hadoop/mapreduce/busiestairports/BusiestAirportsMapper.java
+++ src/cs455/hadoop/mapreduce/busiestairports/BusiestAirportsMapper.java
@@ -1,7 +1,13 @@
 package cs455.hadoop.mapreduce.busiestairports;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class BusiestAirportsMapper extends Mapper {
+// https://stackoverflow.com/questions/31325846/finding-biggest-value-for-key
 
+public class BusiestAirportsMapper extends Mapper<LongWritable, Text, Text, Text> {
+	public void map(LongWritable key, Text value, Context context) {
+		
+	}
 }

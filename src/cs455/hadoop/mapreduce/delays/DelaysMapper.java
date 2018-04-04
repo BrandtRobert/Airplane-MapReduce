a8432a27d08b575c9323c890bd0f96d3a24984fd
Brandt <breutiman@gmail.com>
2018-04-04 17:59:16 -0600

    ads

--- src/cs455/hadoop/mapreduce/delays/DelaysMapper.java
+++ src/cs455/hadoop/mapreduce/delays/DelaysMapper.java
@@ -57,13 +57,13 @@ public class DelaysMapper extends Mapper<LongWritable, Text, Text, Text>  {
 			readFileIntoMap(carriers, carrierToName, 0, 1, context);
 			readFileIntoMap(planeData, tailNumToYear, 0, 8, context);
 			
-			try {
-				context.write(new Text("Airports Map"), new Text ("" + airportToCity.size()));
-				context.write(new Text("Carriers Map"), new Text ("" + tailNumToYear.size()));
-				context.write(new Text("Planes Map"), new Text ("" + carrierToName.size()));
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+//			try {
+//				context.write(new Text("Airports Map"), new Text ("" + airportToCity.size()));
+//				context.write(new Text("Carriers Map"), new Text ("" + tailNumToYear.size()));
+//				context.write(new Text("Planes Map"), new Text ("" + carrierToName.size()));
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
 		}
 	}
 	
@@ -73,45 +73,46 @@ public class DelaysMapper extends Mapper<LongWritable, Text, Text, Text>  {
 		if (lineSplits[0].equals("Year") || lineSplits.length < 29) {
 			return;
 		}
-		// try {
+		 try {
 			List<String> importantFields = new ArrayList<String>(15);
 			Text origin = new Text(lineSplits[16]); // Origin
 			// Associate data
 			String tailNum = lineSplits[10];
 			String manufactureYear = tailNumToYear.get(tailNum);
-			String city = airportToCity.get(origin);
+			String city = airportToCity.get(origin.toString());
 			String carrierCode = lineSplits[8];
 			String carrierName = carrierToName.get(carrierCode);
 			// Year, Month, DofWeek, DepAct, DepSchd, ArrAct, ArrSchd, CarrCode, CarrName, Tail#, ManuYear, City, Dest, CarrDel, WeaDel, NASDel, SecDel, LatDel
 			// Times
-			importantFields.add(lineSplits[0]);		// Year
-			importantFields.add(lineSplits[1]);		// Month
-			importantFields.add(lineSplits[3]); 	// Day of Week
-			importantFields.add(lineSplits[4]); 	// Actual Departure Time
-			importantFields.add(lineSplits[5]);		// Scheduled Departure time
-			importantFields.add(lineSplits[6]); 	// Actual Arrival Time
-			importantFields.add(lineSplits[7]); 	// Scheduled Arrival Time
-			// Identifying information
-			importantFields.add(carrierCode);		// Carrier Code
-			importantFields.add(carrierName);		// Carrier Name
-			importantFields.add(tailNum);			// Tail Num
-			importantFields.add(manufactureYear);	// Aircraft Manufacture Year
-			importantFields.add(city);				// Origin City
-			importantFields.add(lineSplits[17]);	// Destination
-			// Delays
-			importantFields.add(lineSplits[24]);	// Carrier Delay
-			importantFields.add(lineSplits[25]);	// Weather Delay
-			importantFields.add(lineSplits[26]);	// National Air System Delay
-			importantFields.add(lineSplits[27]);	// Security Delay
-			importantFields.add(lineSplits[28]);	// Late Aircraft Delay
+			// importantFields.add(lineSplits[0]);		// Year
+			// importantFields.add(lineSplits[1]);		// Month
+			// importantFields.add(lineSplits[3]); 	// Day of Week
+			// importantFields.add(lineSplits[4]); 	// Actual Departure Time
+			// importantFields.add(lineSplits[5]);		// Scheduled Departure time
+			// importantFields.add(lineSplits[6]); 	// Actual Arrival Time
+			// importantFields.add(lineSplits[7]); 	// Scheduled Arrival Time
+			// // Identifying information
+			// importantFields.add(carrierCode);		// Carrier Code
+			// importantFields.add(carrierName);		// Carrier Name
+			// importantFields.add(tailNum);			// Tail Num
+			// importantFields.add(manufactureYear);	// Aircraft Manufacture Year
+			// importantFields.add(city);				// Origin City
+			// importantFields.add(lineSplits[17]);	// Destination
+			// // Delays
+			// importantFields.add(lineSplits[24]);	// Carrier Delay
+			// importantFields.add(lineSplits[25]);	// Weather Delay
+			// importantFields.add(lineSplits[26]);	// National Air System Delay
+			// importantFields.add(lineSplits[27]);	// Security Delay
+			// importantFields.add(lineSplits[28]);	// Late Aircraft Delay
 			// Combine information into a csv
-			String csv = String.join(",", importantFields);
+			// String csv = String.join(",", importantFields);
+			String csv = manufactureYear + " " + city + " " + carrierName;
 			// City, paired with info for all flights from that city
-		// 	context.write(origin, new Text(csv));
-		// } catch (IOException e) {
-		// 	e.printStackTrace();
-		// } catch (InterruptedException e) {
-		// 	e.printStackTrace();
-		// }
+		 	context.write(origin, new Text(csv));
+		 } catch (IOException e) {
+		 	e.printStackTrace();
+		 } catch (InterruptedException e) {
+		 	e.printStackTrace();
+		 }
 	}
 }

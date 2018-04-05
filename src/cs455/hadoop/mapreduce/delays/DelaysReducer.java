50ac6d913cee4e7a319e32942abefadced31fff4
Brandt <breutiman@gmail.com>
2018-04-04 21:53:21 -0600

    Turns out arrival times weren't recorded the same way as they are now in 1987

--- src/cs455/hadoop/mapreduce/delays/DelaysReducer.java
+++ src/cs455/hadoop/mapreduce/delays/DelaysReducer.java
@@ -9,14 +9,14 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 public class DelaysReducer extends Reducer<Text, Text, Text, Text> {
 	private final CarrierDelayTracker carrierTracker = new CarrierDelayTracker();
-    // Year-0, Month-1, DofWeek-2, DepAct-3, DepSchd-4, ArrAct-5, ArrSchd-6,
-    // CarrCode-7, CarrName-8, Tail#-9, ManuYear-10, City-11, Dest-12, CarrDel-13, 
-    // WeaDel-14, NASDel-15, SecDel-16, LatDel-17
+	// Year-0, Month-1, DofWeek-2, DepAct-3, DepSchd-4, ArrAct-5, ArrSchd-6,
+	// CarrCode-7, CarrName-8, Tail#-9, ManuYear-10, City-11, Dest-12, CarrDel-13, 
+	// WeaDel-14, NASDel-15, SecDel-16, LatDel-17
 	public void reduce (Text airportCode, Iterable<Text> flightRecords, Context context) {
 		for (Text record : flightRecords) {
-			 String [] splits = record.toString().split(",");
-			 int minutesDelayed;
-			 try {
+			String [] splits = record.toString().split(",");
+			int minutesDelayed;
+			try {
 				// Prefer using the carrier delay field, if not available calculate arrival delay
 				if (!splits[13].equals("NA")) {
 					minutesDelayed = Integer.parseInt(splits[13]);
@@ -25,20 +25,13 @@ public class DelaysReducer extends Reducer<Text, Text, Text, Text> {
 					// If the plane arrived before scheluded count it as 0 delay
 					minutesDelayed = Math.max(minutesDelayed, 0);
 				}
-			 } catch (NumberFormatException e) {
-				 try {
-					context.write(new Text("Fields"), record);
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					} catch (InterruptedException e1) {
-						e1.printStackTrace();
-				 }
-				 minutesDelayed = 0;
-			 }
-			 carrierTracker.addDelay(splits[8], minutesDelayed);
+			} catch (NumberFormatException e) {
+				minutesDelayed = 0;
+			}
+			carrierTracker.addDelay(splits[8], minutesDelayed);
 		}
 	}
-	
+
 	public void cleanup(Context context) {
 		try {
 			Map<String, String> delayStats = carrierTracker.getDelayStats();

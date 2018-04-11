80677398a2936b251ae4c7c1f11e78ccf8238def
Brandt <breutiman@gmail.com>
2018-04-10 21:11:24 -0600

    Delete unused import

--- src/cs455/visualization/CancellationMapper.java
+++ src/cs455/visualization/CancellationMapper.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
